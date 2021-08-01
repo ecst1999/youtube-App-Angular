996e0f1112fc96e2991e7ac30d8b500a5543ece1
Steve Carvajal <exactas1999@gmail.com>
2021-08-01 17:42:14 -0500

    Youtube API app con Angular

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  private youtubeURL = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyBtGZHsY4q2-Dgtm8rSGWKHTUplTTlfSSM';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+
+  constructor(private http: HttpClient) { }
+
+  getVideos(){
+
+    const url = `${this.youtubeURL}/playlistItems`;
+
+    const params = new HttpParams()
+          .set('part', 'snippet')
+          .set('maxResults', '10')
+          .set('playlistId', this.playlist)
+          .set('key', this.apikey)
+          .set('pageToken', this.nextPageToken);
+
+    return this.http.get <YoutubeResponse>(url, { params })
+          .pipe(
+
+            map( resp => {
+
+              this.nextPageToken = resp.nextPageToken;
+              return resp.items;
+
+            }),
+
+            map( items =>  items.map( video => video.snippet))
+            
+          );
+
+  }
+
+
+}
